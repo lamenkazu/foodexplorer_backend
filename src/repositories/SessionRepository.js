612edae113cb2742895f946c5e0f7e11b053e2bb
Erick Etiene <333.erick@gmail.com>
2023-12-09 18:22:32 -0300

    🐛 fix: AppError on Session Repository

--- src/repositories/SessionRepository.js
+++ src/repositories/SessionRepository.js
@@ -1,16 +1,18 @@
+const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
 const { compare } = require("bcryptjs");
 const { sign } = require("jsonwebtoken");
 const authConfig = require("../configs/auth");
 
+const errorFromUser = new AppError("E-mail e/ou senha incorreta.", 401);
+
 class SessionRepository {
   async create({ email, password }) {
     const user = await knex("users").where({ email }).first();
-    if (!user) throw new AppError("E-mail e/ou senha incorreta.", 401);
+    if (!user) throw errorFromUser;
 
     const passwordMatched = await compare(password, user.password);
-    if (!passwordMatched)
-      throw new AppError("E-mail e/ou senha incorreta.", 401);
+    if (!passwordMatched) throw errorFromUser;
 
     const { secret, expiresIn } = authConfig.jwt;
     const token = sign({ role: user.role }, secret, {
