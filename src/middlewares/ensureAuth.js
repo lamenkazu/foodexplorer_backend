58d61a6800979462d67d1d68f12574000e427b21
Erick Etiene <333.erick@gmail.com>
2023-12-10 22:32:19 -0300

    ✨ Ensure user authentication && authorization Middlewares

---
+++ src/middlewares/ensureAuth.js
@@ -0,0 +1,35 @@
+const { verify } = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth");
+
+const ensureAuthentication = (req, res, next) => {
+  const cookie = req.headers.cookie;
+  if (!cookie) throw new AppError("JWToken não informado", 401);
+
+  const [, token] = cookie.split("token=");
+
+  try {
+    const { role, sub: user_id } = verify(token, authConfig.jwt);
+
+    req.user = {
+      id: Number(user_id),
+      role,
+    };
+
+    return next();
+  } catch {
+    throw new AppError("Invalid JWT token", 401);
+  }
+};
+
+const ensureAuthorization = (roleToVerify) => {
+  return (req, res, next) => {
+    const { role } = req.user;
+    if (!roleToVerify.includes(role))
+      throw new AppError("Não autorizado.", 401);
+
+    return next();
+  };
+};
+
+module.exports = { ensureAuthorization, ensureAuthentication };
