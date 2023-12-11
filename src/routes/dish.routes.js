8675eaa6a4a0ffd8af1fcc685deaea8631a5451a
Erick Etiene <333.erick@gmail.com>
2023-12-11 20:16:29 -0300

    ✨ Auth in Dish Routes

--- src/routes/dish.routes.js
+++ src/routes/dish.routes.js
@@ -1,14 +1,29 @@
 const { Router } = require("express");
+const {
+  ensureAuthentication,
+  ensureAuthorization,
+} = require("../middlewares/ensureAuth");
 
 const DishesController = require("../controllers/DishesController");
 const dishesController = new DishesController();
 
 const dishesRoutes = Router();
+
+dishesRoutes.use(ensureAuthentication);
+//Everyone
 dishesRoutes.get("/", dishesController.index);
 dishesRoutes.get("/:dish_id", dishesController.show);
 
-dishesRoutes.post("/", dishesController.create);
-dishesRoutes.put("/:dish_id", dishesController.update);
-dishesRoutes.delete("/:dish_id", dishesController.delete);
+dishesRoutes.post("/", ensureAuthorization(["admin"]), dishesController.create);
+dishesRoutes.put(
+  "/:dish_id",
+  ensureAuthorization(["admin"]),
+  dishesController.update
+);
+dishesRoutes.delete(
+  "/:dish_id",
+  ensureAuthorization(["admin"]),
+  dishesController.delete
+);
 
 module.exports = dishesRoutes;
