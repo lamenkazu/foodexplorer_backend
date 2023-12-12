ed29a019d1dc68d6fd98c329e2c86be425c0d7c6
Erick Etiene <333.erick@gmail.com>
2023-12-12 13:22:59 -0300

    Cors Config

--- src/server.js
+++ src/server.js
@@ -12,7 +12,12 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  })
+);
 app.use(routes);
 app.use("files", express.static(dishImgConfig.UPLOADS_MENU_FOLDER));
 
