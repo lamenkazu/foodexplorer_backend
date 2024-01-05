2383c237e21c22f04b0e168cdc5a9754dda6e58f
Erick Etiene <333.erick@gmail.com>
2024-01-05 12:37:51 -0300

    ✏️ Allow Netlify Deploy to use DB

--- src/server.js
+++ src/server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://foodexplorer-frontend-3cc695.netlify.app",
+    ],
     credentials: true,
   })
 );
